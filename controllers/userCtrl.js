c0fe0a0083d2fe462d82e140e814058095fa96b1
Tehman Sardar <tehmansardar@gmail.com>
2021-02-03 13:07:38 +0500

    Client Build

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -2,6 +2,9 @@ const { findOne } = require('../models/userModel');
 const User = require('../models/userModel');
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { CLIENT_URL } = process.env;
 
 const userCtrl = {
 	register: async (req, res) => {
@@ -23,9 +26,19 @@ const userCtrl = {
 					.status(400)
 					.json({ msg: 'Password must be atleast 6 characters' });
 
-			// const passwordHash =
+			const passwordHash = await bcrypt.hash(password, 12);
 
-			console.log(name, email, password);
+			const newUser = {
+				name,
+				email,
+				password: passwordHash,
+			};
+
+			const activation_token = createActivationToken(newUser);
+
+			const url = `${CLIENT_URL}/user/activate/${activation_token}`;
+
+			console.log(activation_token);
 		} catch (error) {
 			return res.status(500).json({ msg: error.message });
 		}
@@ -36,5 +49,23 @@ function validateEmail(email) {
 	const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email);
 }
+
+const createActivationToken = (payload) => {
+	return jwt.sign(payload, process.env.ACTIVATION_TOKEN_SECRET, {
+		expiresIn: '5m',
+	});
+};
+
+const createAccessToken = (payload) => {
+	return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+		expiresIn: '15m',
+	});
+};
+
+const createRefreshToken = (payload) => {
+	return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+		expiresIn: '7d',
+	});
+};
 
 module.exports = userCtrl;
