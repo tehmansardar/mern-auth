99f9a4a5dd89f670a2373a862ad1472ae2632a1f
Tehman Sardar <tehmansardar@gmail.com>
2021-02-26 05:06:14 +0500

    Client Activation Email

--- client/src/components/body/Body.js
+++ client/src/components/body/Body.js
@@ -3,12 +3,20 @@ import { Switch, Route } from 'react-router-dom';
 
 import Login from './auth/Login';
 import Register from './auth/Register';
+import ActivationEmail from './auth/ActivationEmail';
+
 const Body = () => {
 	return (
 		<section>
 			<Switch>
 				<Route path='/login' component={Login} exact />
 				<Route path='/register' component={Register} exact />
+
+				<Route
+					path='/user/activate/:activation_token'
+					component={ActivationEmail}
+					exact
+				/>
 			</Switch>
 		</section>
 	);
